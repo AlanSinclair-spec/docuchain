8ed9d83a15d1556568dc2c9910a8daebaf02daf5
William Menjivar <williammenjivar@MacBookPro.lan>
2025-08-29 17:46:33 -0700

    Update Next.js config for better Vercel deployment compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
+  trailingSlash: false,
+  output: 'standalone',
 }
 
 export default nextConfig
